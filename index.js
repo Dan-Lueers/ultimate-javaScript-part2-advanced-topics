59d7573db834e0d1103f0f1904561889eca5c490
Dan-Lueers <dan.lueers@gmail.com>
2025-06-26 22:34:30 +0200

    private properties and methods

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// --------------------------------------
+// Factory Functions vs Constructor Functions
+// --------------------------------------
+
 // Factory Functions
 function createCircle(radius) {
   return {
@@ -22,15 +26,32 @@ function Circle(radius) {
 let circle2 = new Circle(2);
 circle2.draw();
 
-
+// --------------------------------------
 // Enumerating over properties
-for(key in circle1) {
-    if(typeof circle1[key] !== 'function')
-        console.log(key, circle1[key])
-} 
+// --------------------------------------
+for (let key in circle1) {
+  if (typeof circle1[key] !== "function") console.log(key, circle1[key]);
+}
 
 const keys = Object.keys(circle1);
 console.log(keys);
 
-if('radius' in circle1)
-    console.log('circle has radius')
+if ("radius" in circle1) console.log("circle has radius");
+
+// --------------------------------------
+// Private Properties and Methods
+// --------------------------------------
+function MyCircle(radius) {
+  this.radius = radius;
+  let defaultLocation = { x: 1, y: 1 };
+  let computeOptimumLocation = function () {
+    console.log("computing optimal location...");
+  };
+  this.draw = function () {
+    console.log("draw Mycircle from constructor function");
+    computeOptimumLocation();
+  };
+}
+
+const myCircle = new MyCircle();
+myCircle.draw();
